da647a5a97e22705e22886e9c4ce5edb7f4277e1
Brody Kenrick <user.fake@server.userfake>
2013-12-13 14:48:22 +1100

    Correct for missing exit status in print and execute script

--- travis_exec_and_print_time.sh
+++ travis_exec_and_print_time.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo "Exec and print time periodically (overcoming our very long quiet times)"
+echo "Exec and print time periodically - overcoming long quiet times"
 #Test command
 #CMD="sleep 5"
 #echo "CMD=$CMD"
@@ -15,13 +15,20 @@ PID=$!
 echo "Testing for PID=$PID"
 
 #Sleep for X seconds and then test if the PID is still active
-# inefficient but not too bad for this use case
+# inefficient (only breaks in intervals of timer) but not too bad for this use case
 # also not safe from race conditions in general but fine here
-while sleep 180
-      kill -0 $PID >/dev/null 2>&1
+while  ps | grep " $PID " | grep -v grep 2>&1
 do
-    echo "Still running at $SECONDS seconds"
+    sleep 180
+    echo "PID=$PID still running at $SECONDS seconds"
 done
 
 echo "Ran for $SECONDS seconds"
+
+#Wait will get the process even if it has already exited in this session
+#echo "Waiting on PID=$PID"
+wait $PID
+STATUS=$?
+echo "Exit status of $STATUS"
+exit $STATUS
 
