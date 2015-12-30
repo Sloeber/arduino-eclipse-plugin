73cda873432f43c8f3dd498432539865b1f76e2e
jantje <jan@baeyens.it>
2015-12-30 22:23:34 +0100

    Something was missing in the previous commit

--- it.baeyens.arduino.common/src/it/baeyens/arduino/common/Common.java
+++ it.baeyens.arduino.common/src/it/baeyens/arduino/common/Common.java
@@ -2,7 +2,6 @@ package it.baeyens.arduino.common;
 
 import java.io.File;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.Vector;
 
 import org.eclipse.cdt.core.CCorePlugin;
@@ -11,30 +10,19 @@ import org.eclipse.cdt.core.envvar.IContributedEnvironment;
 import org.eclipse.cdt.core.envvar.IEnvironmentVariable;
 import org.eclipse.cdt.core.envvar.IEnvironmentVariableManager;
 import org.eclipse.cdt.core.model.CoreModel;
-import org.eclipse.cdt.core.model.ICElement;
-import org.eclipse.cdt.core.model.ICProject;
 import org.eclipse.cdt.core.settings.model.ICConfigurationDescription;
 import org.eclipse.cdt.core.settings.model.ICProjectDescription;
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.QualifiedName;
 import org.eclipse.core.runtime.Status;
-import org.eclipse.jface.text.ITextSelection;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.ui.IEditorPart;
-import org.eclipse.ui.IViewPart;
-import org.eclipse.ui.IViewReference;
 import org.eclipse.ui.IWorkbenchPage;
-import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.statushandlers.StatusManager;
@@ -217,18 +205,6 @@ public class Common extends ArduinoInstancePreferences {
 	return Integer.parseInt(Number.trim());
     }
 
-    private static ICConfigurationDescription[] getCfgs(IProject prj) {
-	ICProjectDescription prjd = CoreModel.getDefault().getProjectDescription(prj, false);
-	if (prjd != null) {
-	    ICConfigurationDescription[] cfgs = prjd.getConfigurations();
-	    if (cfgs != null) {
-		return cfgs;
-	    }
-	}
-
-	return new ICConfigurationDescription[0];
-    }
-
     public static IWorkbenchWindow getActiveWorkbenchWindow() {
 	return PlatformUI.getWorkbench().getActiveWorkbenchWindow();
     }
@@ -241,205 +217,7 @@ public class Common extends ArduinoInstancePreferences {
 	return null;
     }
 
-    /**
-     * Class used to efficiently special case the scenario where there's only a single project in the workspace. See bug 375760
-     */
-    private static class ImaginarySelection implements ISelection {
-	private IProject fProject;
-
-	ImaginarySelection(IProject project) {
-	    this.fProject = project;
-	}
-
-	@Override
-	public boolean isEmpty() {
-	    return this.fProject == null;
-	}
-
-	IProject getProject() {
-	    return this.fProject;
-	}
-    }
-
     static HashSet<IProject> fProjects = new HashSet<>();
-
-    static public IProject[] getSelectedProjects() {
-	fProjects.clear();
-	getSelectedProjects(getActiveWorkbenchWindow().getSelectionService().getSelection());
-	return fProjects.toArray(new IProject[fProjects.size()]);
-    }
-
-    static private void getSelectedProjects(ISelection selection) {
-
-	boolean badObject = false;
-
-	if (selection != null) {
-	    if (selection instanceof IStructuredSelection) {
-		if (selection.isEmpty()) {
-		    // could be a form editor or something. try to get the
-		    // project from the active part
-		    IWorkbenchPage page = getActivePage();
-		    if (page != null) {
-			IWorkbenchPart part = page.getActivePart();
-			if (part != null) {
-			    Object o = part.getAdapter(IResource.class);
-			    if (o != null && o instanceof IResource) {
-				fProjects.add(((IResource) o).getProject());
-			    }
-			}
-		    }
-		}
-		Iterator<?> iter = ((IStructuredSelection) selection).iterator();
-		while (iter.hasNext()) {
-		    Object selItem = iter.next();
-		    IProject project = null;
-		    if (selItem instanceof ICElement) {
-			ICProject cproject = ((ICElement) selItem).getCProject();
-			if (cproject != null)
-			    project = cproject.getProject();
-		    } else if (selItem instanceof IResource) {
-			project = ((IResource) selItem).getProject();
-		    } else if (selItem instanceof IAdaptable) {
-			Object adapter = ((IAdaptable) selItem).getAdapter(IProject.class);
-			if (adapter != null && adapter instanceof IProject) {
-			    project = (IProject) adapter;
-			}
-		    }
-		    // Check whether the project is CDT project
-		    if (project != null) {
-			if (!CoreModel.getDefault().isNewStyleProject(project))
-			    project = null;
-			else {
-			    ICConfigurationDescription[] tmp = getCfgs(project);
-			    if (tmp.length == 0)
-				project = null;
-			}
-		    }
-		    if (project != null) {
-			fProjects.add(project);
-		    } else {
-			badObject = true;
-			break;
-		    }
-		}
-	    } else if (selection instanceof ITextSelection) {
-		// If a text selection check the selected part to see if we can
-		// find
-		// an editor part that we can adapt to a resource and then
-		// back to a project.
-		IWorkbenchWindow window = getActiveWorkbenchWindow();
-		if (window != null) {
-		    IWorkbenchPage page = window.getActivePage();
-		    if (page != null) {
-			IWorkbenchPart part = page.getActivePart();
-			if (part instanceof IEditorPart) {
-			    IEditorPart epart = (IEditorPart) part;
-			    @SuppressWarnings("cast") // cast is needed to build with oracle jdk 8
-			    IResource resource = (IResource) epart.getEditorInput().getAdapter(IResource.class);
-			    if (resource != null) {
-				IProject project = resource.getProject();
-				badObject = !(project != null && CoreModel.getDefault().isNewStyleProject(project));
-
-				if (!badObject) {
-				    fProjects.add(project);
-				}
-			    }
-			}
-		    }
-		}
-
-	    } else if (selection instanceof ImaginarySelection) {
-		fProjects.add(((ImaginarySelection) selection).getProject());
-	    }
-	}
-
-	if (!badObject && !fProjects.isEmpty()) {
-	    Iterator<IProject> iter = fProjects.iterator();
-	    ICConfigurationDescription[] firstConfigs = getCfgs(iter.next());
-	    if (firstConfigs != null) {
-		for (ICConfigurationDescription firstConfig : firstConfigs) {
-		    boolean common = true;
-		    Iterator<IProject> iter2 = fProjects.iterator();
-		    while (iter2.hasNext()) {
-			ICConfigurationDescription[] currentConfigs = getCfgs(iter2.next());
-			int j = 0;
-			for (; j < currentConfigs.length; j++) {
-			    if (firstConfig.getName().equals(currentConfigs[j].getName()))
-				break;
-			}
-			if (j == currentConfigs.length) {
-			    common = false;
-			    break;
-			}
-		    }
-		    if (common) {
-			break;
-		    }
-		}
-	    }
-	}
-	// action.setEnabled(enable);
-
-	// Bug 375760
-	// If focus is on a view that doesn't provide a resource/project
-	// context. Use the selection in a
-	// project/resource view. We support three views. If more than one is
-	// open, nevermind. If there's only
-	// one project in the workspace and it's a CDT one, use it
-	// unconditionally.
-	//
-	// Note that whatever project we get here is just a candidate; it's
-	// tested for suitability when we
-	// call ourselves recursively
-	//
-	if (badObject || fProjects.isEmpty()) {
-	    // Check for lone CDT project in workspace
-	    IProject[] projects = ResourcesPlugin.getWorkspace().getRoot().getProjects();
-	    if (projects != null && projects.length == 1) {
-		IProject project = projects[0];
-		if (CoreModel.getDefault().isNewStyleProject(project) && (getCfgs(project).length > 0)) {
-		    getSelectedProjects(new ImaginarySelection(project));
-		    return;
-		}
-	    }
-
-	    // Check the three supported views
-	    IWorkbenchPage page = getActivePage();
-	    int viewCount = 0;
-	    if (page != null) {
-		IViewReference theViewRef = null;
-		IViewReference viewRef = null;
-
-		theViewRef = page.findViewReference("org.eclipse.cdt.ui.CView"); //$NON-NLS-1$
-		viewCount += (theViewRef != null) ? 1 : 0;
-
-		viewRef = page.findViewReference("org.eclipse.ui.navigator.ProjectExplorer"); //$NON-NLS-1$
-		viewCount += (viewRef != null) ? 1 : 0;
-		theViewRef = (theViewRef == null) ? viewRef : theViewRef;
-
-		viewRef = page.findViewReference("org.eclipse.ui.views.ResourceNavigator"); //$NON-NLS-1$
-		viewCount += (viewRef != null) ? 1 : 0;
-		theViewRef = (theViewRef == null) ? viewRef : theViewRef;
-
-		if (theViewRef != null && viewCount >= 1) {
-		    IViewPart view = theViewRef.getView(false);
-		    if (view != null) {
-			ISelection cdtSelection = view.getSite().getSelectionProvider().getSelection();
-			if (cdtSelection != null) {
-			    if (!cdtSelection.isEmpty()) {
-				if (!cdtSelection.equals(selection)) { // avoids
-								       // infinite
-								       // recursion
-				    getSelectedProjects(cdtSelection);
-				    return;
-				}
-			    }
-			}
-		    }
-		}
-	    }
-	}
-    }
 
     public static boolean StopSerialMonitor(String mComPort) {
 	if (OtherSerialUser != null) {
---
+++ it.baeyens.arduino.core/src/it/baeyens/arduino/listeners/ProjectExplorerListener.java
@@ -0,0 +1,62 @@
+package it.baeyens.arduino.listeners;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.ISelectionListener;
+import org.eclipse.ui.ISelectionService;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.PlatformUI;
+
+public class ProjectExplorerListener implements ISelectionListener {
+    static IProject projects[] = new IProject[0];
+
+    // public static IProject getActiveProject() {
+    // return project;
+    // }
+
+    public static IProject[] getSelectedProjects() {
+	return projects;
+    }
+
+    public static void registerListener() {
+	ISelectionService ss = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getSelectionService();
+
+	ProjectExplorerListener selectionListener = new ProjectExplorerListener();
+	ss.addPostSelectionListener(IPageLayout.ID_PROJECT_EXPLORER, selectionListener);
+
+    }
+
+    @Override
+    public void selectionChanged(IWorkbenchPart part, ISelection newSelection) {
+	if (newSelection instanceof IStructuredSelection) {
+	    // Object element = ((IStructuredSelection) newSelection).getFirstElement();
+	    // if (element instanceof IAdaptable) {
+	    // IResource resource = ((IAdaptable) element).getAdapter(IResource.class);
+	    // project = resource.getProject();
+	    //
+	    // // do your action here!!
+	    // }
+
+	    List<IProject> allSelectedprojects = new ArrayList<>();
+	    for (Object element : ((IStructuredSelection) newSelection).toList()) {
+		if (element instanceof IAdaptable) {
+		    IResource resource = ((IAdaptable) element).getAdapter(IResource.class);
+		    allSelectedprojects.add(resource.getProject());
+
+		}
+	    }
+	    projects = new IProject[allSelectedprojects.size()];
+	    allSelectedprojects.toArray(projects);
+	    return;
+	}
+
+    }
+
+}
