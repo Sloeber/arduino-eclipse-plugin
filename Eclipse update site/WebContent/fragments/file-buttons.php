6ef393ebdd454d6b6dff4fc65486b37c3a8950c5
jantje <eclipse@baeyens.it>
2016-04-02 00:22:45 +0200

    Making stable download pages 1 dynamic page
    Updated file-buttons so it finds the 32 and 64 in the name even wint win
    and lin.
    stable.php should do the trick now.

--- Eclipse update site/WebContent/fragments/file-buttons.php
+++ Eclipse update site/WebContent/fragments/file-buttons.php
@@ -1,43 +1,38 @@
 <?php
-  if(isset($_GET["ver"]) && isset($_GET["arch"])) 
-    listFiles("V" . $_GET["ver"] . '_' . $_GET["arch"]);
-
-  function listFiles($prefix) {
-    global $filter;
-    $filter = $prefix;
-    date_default_timezone_set('UTC');
-    $location="/customers/0/9/d/baeyens.it/httpd.www/eclipse/download/product";
-    echo "<!-- listing files in $location with prefix $prefix -->";
-    $dir = opendir($location);
-    while(false != ($file = readdir($dir))) {
-      if(($file != ".") and ($file != "..") and ($file != "index.php")) {
-        $files[] = $location."/".$file; // put in array.
-      }
-    }
-    closedir($dir);
-    $files = array_filter($files, "filter");
-    sort($files);
-    $count = count($files);
-    if ($count == 1) {
-    	$curfile=basename($files[0]);
-      echo '<div class="text-center col-md-4 col-md-offset-4">';
-      echo '  <a href="http://eclipse.baeyens.it/download/product/' . $curfile . '?forcedownload" class="btn btn-success btn-lg text-center">Download <b>' . substr($curfile, strlen($prefix), 2) . ' bits</b> Bundle <i class="glyphicon glyphicon-cloud-download"></i></a>';
-      echo '</div>';
-    } else if ($count == 2) {
-    	$curfile=basename($files[0]);
-      echo '<div class="text-center col-md-3 col-md-offset-3">';
-      echo '  <a href="http://eclipse.baeyens.it/download/product/' . $curfile . '?forcedownload" class="btn btn-success btn-lg text-center">Download <b>' . substr($curfile, strlen($prefix), 2) . ' bits</b> Bundle <i class="glyphicon glyphicon-cloud-download"></i></a>';
-      echo '</div>';
-      $curfile=basename($files[1]);
-      echo '<div class="text-center col-md-3">';
-      echo '  <a href="http://eclipse.baeyens.it/download/product/' . $curfile . '?forcedownload" class="btn btn-success btn-lg text-center">Download <b>' . substr($curfile, strlen($prefix), 2) . ' bits</b> Bundle <i class="glyphicon glyphicon-cloud-download"></i></a>';
-      echo '</div>';
-    } else {
-      echo "<!-- there are $count files in $location with prefix $prefix -->";
-    }
-  }
-  function filter($file) {
-    global $filter;
-    return(substr(basename($file), 0, strlen($filter)) == $filter);
-  }
+if (isset ( $_GET ["ver"] ) && isset ( $_GET ["arch"] ))
+	listFiles ( "V" . $_GET ["ver"] . '_' . $_GET ["arch"] );
+function my_offset($curfile, $prefix) {
+	preg_match ( '/\d/', $curfile, $m, PREG_OFFSET_CAPTURE, strlen ( $prefix ) );
+	if (sizeof ( $m ))
+		return $m [0] [1];
+	return strlen ( $prefix );
+}
+function my_bitness($curfile, $prefix) {
+	return substr ( $curfile, my_offset ( $curfile, $prefix ), 2 );
+}
+function listFiles($prefix) {
+	global $filter;
+	$filter = $prefix;
+	date_default_timezone_set ( 'UTC' );
+	$location = "/customers/0/9/d/baeyens.it/httpd.www/eclipse/download/product";
+	$dir = opendir ( $location );
+	while ( false != ($file = readdir ( $dir )) ) {
+		if (($file != ".") and ($file != "..") and ($file != "index.php")) {
+			$files [] = $location . "/" . $file; // put in array.
+		}
+	}
+	closedir ( $dir );
+	$files = array_filter ( $files, "filter" );
+	sort ( $files );
+	foreach ( $files as &$file ) {
+		$curfile = basename ( $file );
+		echo '<div class="text-center col-md-4 col-md-offset-4">';
+		echo '  <a href="http://eclipse.baeyens.it/download/product/' . $curfile . '" class="btn btn-success btn-lg text-center">Download <b>' . my_bitness ( $curfile, $prefix ) . ' bits</b> Bundle <i class="glyphicon glyphicon-cloud-download"></i></a>';
+		echo '</div>';
+	}
+}
+function filter($file) {
+	global $filter;
+	return (substr ( basename ( $file ), 0, strlen ( $filter ) ) == $filter);
+}
 ?>
