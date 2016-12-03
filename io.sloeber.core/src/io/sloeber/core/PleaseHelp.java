32e32166af9ab7cc949d6507073c4ea0560756b7
jantje <eclipse@baeyens.it>
2016-12-04 00:45:12 +0100

    The php seperator is & and not ;

--- io.sloeber.core/src/io/sloeber/core/PleaseHelp.java
+++ io.sloeber.core/src/io/sloeber/core/PleaseHelp.java
@@ -18,83 +18,83 @@ import org.osgi.framework.FrameworkUtil;
 
 public class PleaseHelp extends Dialog {
 
-    public Browser browser = null;
-    public PleaseHelp ph;
-    private static String myhelpLocation;
+	public Browser browser = null;
+	public PleaseHelp ph;
+	private static String myhelpLocation;
 
-    public PleaseHelp(Shell parent) {
-	super(parent);
-	this.ph = this;
-    }
-
-    @Override
-    protected void handleShellCloseEvent() {// JABA is not going to add code
-    }
-
-    @Override
-    protected Control createContents(Composite parent) {
-	GridLayout gridLayout = new GridLayout();
-	gridLayout.numColumns = 3;
-	parent.setLayout(gridLayout);
-
-	try {
-	    this.browser = new Browser(parent, SWT.NONE);
-	} catch (SWTError e) {
-	    System.out.println("Could not instantiate Browser: " + e.getMessage()); //$NON-NLS-1$
+	public PleaseHelp(Shell parent) {
+		super(parent);
+		this.ph = this;
 	}
-	GridData data = new GridData();
-	data.horizontalAlignment = GridData.FILL;
-	data.verticalAlignment = GridData.FILL;
-	data.horizontalSpan = 3;
-	data.grabExcessHorizontalSpace = true;
-	data.grabExcessVerticalSpace = true;
-	this.browser.setLayoutData(data);
 
-	this.browser.addProgressListener(new ProgressListener() {
-	    @Override
-	    public void changed(ProgressEvent event) {
-		// I'm not interested in change
-	    }
+	@Override
+	protected void handleShellCloseEvent() {// JABA is not going to add code
+	}
 
-	    @Override
-	    public void completed(ProgressEvent event) {
-		PleaseHelp.this.ph.setBlockOnOpen(false);
-		PleaseHelp.this.ph.open();
-	    }
-	});
+	@Override
+	protected Control createContents(Composite parent) {
+		GridLayout gridLayout = new GridLayout();
+		gridLayout.numColumns = 3;
+		parent.setLayout(gridLayout);
 
-	this.buttonBar = createButtonBar(parent);
+		try {
+			this.browser = new Browser(parent, SWT.NONE);
+		} catch (SWTError e) {
+			System.out.println("Could not instantiate Browser: " + e.getMessage()); //$NON-NLS-1$
+		}
+		GridData data = new GridData();
+		data.horizontalAlignment = GridData.FILL;
+		data.verticalAlignment = GridData.FILL;
+		data.horizontalSpan = 3;
+		data.grabExcessHorizontalSpace = true;
+		data.grabExcessVerticalSpace = true;
+		this.browser.setLayoutData(data);
 
-	org.osgi.framework.Version version = FrameworkUtil.getBundle(getClass()).getVersion();
-	String url = myhelpLocation + "?os=" + Platform.getOS(); //$NON-NLS-1$
-	url += ";arch=" + Platform.getOSArch(); //$NON-NLS-1$
-	url += ";majorVersion=" + version.getMajor(); //$NON-NLS-1$
-	url += ";minorVersion=" + version.getMinor(); //$NON-NLS-1$
-	url += ";microVersion=" + version.getMicro(); //$NON-NLS-1$
-	url += ";qualifierVersion=" + version.getQualifier(); //$NON-NLS-1$
-	this.browser.setUrl(url);
+		this.browser.addProgressListener(new ProgressListener() {
+			@Override
+			public void changed(ProgressEvent event) {
+				// I'm not interested in change
+			}
 
-	return parent;
+			@Override
+			public void completed(ProgressEvent event) {
+				PleaseHelp.this.ph.setBlockOnOpen(false);
+				PleaseHelp.this.ph.open();
+			}
+		});
 
-    }
+		this.buttonBar = createButtonBar(parent);
 
-    /**
-     * Static helper function to popup the help jantje page
-     * 
-     */
-    static void doHelp(String helpLocation) {
-	myhelpLocation = helpLocation;
-	PlatformUI.getWorkbench().getDisplay().asyncExec(new Runnable() {
-	    @Override
-	    public void run() {
-		Shell activeShell = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
-		new PleaseHelp(activeShell).create();
-	    }
-	});
-    }
+		org.osgi.framework.Version version = FrameworkUtil.getBundle(getClass()).getVersion();
+		String url = myhelpLocation + "?os=" + Platform.getOS(); //$NON-NLS-1$
+		url += "&arch=" + Platform.getOSArch(); //$NON-NLS-1$
+		url += "&majorVersion=" + version.getMajor(); //$NON-NLS-1$
+		url += "&minorVersion=" + version.getMinor(); //$NON-NLS-1$
+		url += "&microVersion=" + version.getMicro(); //$NON-NLS-1$
+		url += "&qualifierVersion=" + version.getQualifier(); //$NON-NLS-1$
+		this.browser.setUrl(url);
 
-    @Override
-    protected Point getInitialSize() {
-	return this.getShell().computeSize(SWT.MAX, SWT.MAX, true);
-    }
+		return parent;
+
+	}
+
+	/**
+	 * Static helper function to popup the help jantje page
+	 *
+	 */
+	static void doHelp(String helpLocation) {
+		myhelpLocation = helpLocation;
+		PlatformUI.getWorkbench().getDisplay().asyncExec(new Runnable() {
+			@Override
+			public void run() {
+				Shell activeShell = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
+				new PleaseHelp(activeShell).create();
+			}
+		});
+	}
+
+	@Override
+	protected Point getInitialSize() {
+		return this.getShell().computeSize(SWT.MAX, SWT.MAX, true);
+	}
 }
