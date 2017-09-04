7cf5557be474fdd2e88133d194677443f2b35516
jantje <eclipse@baeyens.it>
2017-09-04 16:03:47 +0200

    only call gihub once every  10 minutes
    Also added error reporting

--- website/WebContent/github_status.php
+++ website/WebContent/github_status.php
@@ -1,19 +1,4 @@
 <!DOCTYPE html>
-<?php
-$MAJOR_VERSION = "0";
-$MINOR_VERSION = "0";
-$MICRO_VERSION = "0";
-$QUALIFIER_VERSION = "0";
-
-if (isset ( $_GET ["majorVersion"] ))
-    $MAJOR_VERSION = filter_var(trim($_GET ["majorVersion"]), FILTER_SANITIZE_NUMBER_INT);
-if (isset ( $_GET ["minorVersion"] ))
-    $MINOR_VERSION = filter_var(trim($_GET ["minorVersion"]), FILTER_SANITIZE_NUMBER_INT);
-if (isset ( $_GET ["microVersion"] ))
-    $MICRO_VERSION = filter_var(trim($_GET ["microVersion"]), FILTER_SANITIZE_NUMBER_INT);
-if (isset ( $_GET ["qualifierVersion"] ))
-    $QUALIFIER_VERSION = filter_var(trim($_GET ["qualifierVersion"]), FILTER_SANITIZE_NUMBER_INT);
-?>
 
 
 <html lang="en">
@@ -47,7 +32,7 @@ if (isset ( $_GET ["qualifierVersion"] ))
 <style>
 table, th, td {
     border: 1px solid black;
-	padding: 2px;
+        padding: 2px;
 }
 
 th {
@@ -64,31 +49,42 @@ th {
     </nav>
 
 
-<?php
-function cachedGithubJson($cachefile, $url) {
-//    echo "geting cached data from url: <a href=\"" . $url . "\">" . $url . "</a><br>\n";
-//    echo "Testing existance of " . $cachefile . "<br/>\n";
 
-        $ret="";
-    if (file_exists ( $cachefile )) {
-//        echo $cachefile . " exists<br/>\n";
-        $CacheFileDate = filectime ( $cachefile );
-        if (time () <= $CacheFileDate + (60 * 20)) {
-            $ret = file_get_contents ( $cachefile );
-            if (strlen ( $ret ) > 10) {
-//                echo "using cache<br/>\n";
-            } else {
-//                echo "Local cache is empty<br/>\n";
-                $ret = "";
-            }
-        } else {
-//            echo $cachefile . " Is old<br/>\n";
+<?php
+$MAJOR_VERSION = "0";
+$MINOR_VERSION = "0";
+$MICRO_VERSION = "0";
+$QUALIFIER_VERSION = "0";
+$debuginfo = "<strong>Debug output </strong><br>\n";
+$SHOW_DEBUG_INFO = false;
+$enabledump = false;
+
+
+if (isset ( $_GET ["majorVersion"] ))
+    $MAJOR_VERSION = filter_var(trim($_GET ["majorVersion"]), FILTER_SANITIZE_NUMBER_INT);
+if (isset ( $_GET ["minorVersion"] ))
+    $MINOR_VERSION = filter_var(trim($_GET ["minorVersion"]), FILTER_SANITIZE_NUMBER_INT);
+if (isset ( $_GET ["microVersion"] ))
+    $MICRO_VERSION = filter_var(trim($_GET ["microVersion"]), FILTER_SANITIZE_NUMBER_INT);
+if (isset ( $_GET ["qualifierVersion"] ))
+    $QUALIFIER_VERSION = filter_var(trim($_GET ["qualifierVersion"]), FILTER_SANITIZE_NUMBER_INT);
+
+function onlyhitgithubEveryFiveMinutes($url){
+    global $debuginfo;
+
+    $delayTime =(60 * 10 );
+    $githubFile="./cache/githubhit.tmp";
+      if (file_exists ( $githubFile )){
+            $GithubHitDate = filectime ( $githubFile );
+        if (time () <= ($GithubHitDate + $delayTime )) {
+            $debuginfo= $debuginfo .  $cachefile . "<strong>github url request is ignored</strong>. " . $url ."<br>\n";
+            $waittime=$GithubHitDate + $delayTime- time ();
+             $debuginfo= $debuginfo .  "<strong>Please wait. " . $waittime . " seconds.</strong><br>\n";
+            return false;
         }
-    } else {
-//        echo $cachefile . " does not exist<br/>\n";
-    }
-    if ($ret == "") {
-        // create a context to use, so github won't complain about missing user-agent
+      }
+      $debuginfo= $debuginfo .  $cachefile . "github url rquest is actioned. ". $url ."<br>\n";
+            // create a context to use, so github won't complain about missing user-agent
         $opts = [
                 'http' => [
                         'method' => 'GET',
@@ -98,62 +94,103 @@ function cachedGithubJson($cachefile, $url) {
                 ]
         ];
         $context = stream_context_create($opts);
-
+        file_put_contents($githubFile,time ());
         $ret = file_get_contents ( $url, false, $context);
         if ($ret === false) {
-            echo "<strong>WARNING: Download of ".$url." failed - using cached data instead.</strong><br>\n";
+
+            $debuginfo= $debuginfo . "<strong>WARNING: Download of ".$url." failed - using cached data instead.</strong><br>\n";
+        }
+        return $ret;
+
+
+}
+function cachedGithubJson($cachefile, $url) {
+    global  $debuginfo ,$enabledump;
+    $debuginfo= $debuginfo . "get data from url: <a href=\"" . $url . "\">" . $url . "</a><br>\n";
+    $cachefile = "./cache/".$cachefile;
+
+    $ret="";
+    if (file_exists ( $cachefile )) {
+        $debuginfo= $debuginfo .  $cachefile . " exists<br/>\n";
+        $CacheFileDate = filectime ( $cachefile );
+        if (time () <= $CacheFileDate + (60 * 60 * 10)) {
             $ret = file_get_contents ( $cachefile );
+            if (strlen ( $ret ) > 10) {
+                $debuginfo= $debuginfo .  "    using cache<br/>\n";
+            } else {
+                $debuginfo= $debuginfo .  "    Local cache is empty<br/>\n";
+                $ret = "";
+            }
         } else {
-            file_put_contents ( $cachefile, $ret );
+            $debuginfo= $debuginfo .  $cachefile . " Cache is out of date<br/>\n";
+        }
+    } else {
+        $debuginfo= $debuginfo .  $cachefile . " does not exist<br/>\n";
+    }
+    if ($ret == "") {
+
+        $ret=onlyhitgithubEveryFiveMinutes($url);
+        if ($ret === false) {
+
+            $ret = file_get_contents ( $cachefile );
+            $enabledump = true;
+        }
+        else{
+            file_put_contents($cachefile,$ret);
         }
     }
+      $debuginfo= $debuginfo .  "<br/>\n";
     return $ret;
 }
 
 function dumpIssues($version, $issues){
-	// output the td with the version
-	$numIssues = count($issues);
-	$firstRowOnly= "<td rowspan=".$numIssues." >".$version."</td>";
+        // output the td with the version
+        $numIssues = count($issues);
+        $firstRowOnly= "<td rowspan=".$numIssues." >".$version."</td>";
 
-	foreach ( $issues as $issue ) {
-		$title = "<a href=" . $issue ["html_url"] . ">" . htmlspecialchars($issue ["title"]) . "</a> ";
-		$domain_label = "";
-		$importance_label = "";
-		$os_label = "";
-		$rest_label = "";
-		$importance_inbetween = "";
-		$domain_inbetween = "";
-		$os_inbetween = "";
-		$rest_inbetween = "";
+        foreach ( $issues as $issue ) {
+                $title = "<a href=" . $issue ["html_url"] . ">" . htmlspecialchars($issue ["title"]) . "</a> ";
+                $domain_label = "";
+                $importance_label = "";
+                $os_label = "";
+                $rest_label = "";
+                $importance_inbetween = "";
+                $domain_inbetween = "";
+                $os_inbetween = "";
+                $rest_inbetween = "";
 
-		foreach ( $issue ["labels"] as $label ) {
-			if (0 === strpos ( $label ["name"], "domain: " )) {
-				$domain_label = $domain_label . $domain_inbetween . '<SPAN style="background-color: #' . $label ["color"] . ';"> <b>' . htmlspecialchars(substr ( $label ["name"], 8 )) . " </b></SPAN>&ensp;";
-				$domain_inbetween = "<br>";
-			} elseif (0 === strpos ( $label ["name"], "importance: " )) {
-				$importance_label = $importance_label . $importance_inbetween . '<SPAN style="background-color: #' . $label ["color"] . ';"> <b>' . htmlspecialchars(substr ( $label ["name"], 12 )) . " </b></SPAN>&ensp;";
-				$importance_inbetween = "<br>";
-			} elseif (0 === strpos ( $label ["name"], "OS: " )) {
-				$os_label = $os_label . $os_inbetween .'<SPAN style="background-color: #' . $label ["color"] . ';"> <b>' . htmlspecialchars(substr ( $label ["name"], 4 )) . " </b></SPAN>&ensp;";
-				$os_inbetween = "<br>";
-			} elseif (0 === strpos($label["name"], "status: fixed in")){
-			  // ignore fixed status as this is in first column already
-			} else {
-				$rest_label = $rest_label . $rest_inbetween . '<SPAN style="background-color: #' . $label ["color"] . ';"> <b>' . htmlspecialchars($label ["name"]) . " </b></SPAN>&ensp;";
-				$rest_inbetween = "<br>";
-			}
-		}
-		echo "<tr>";
-		echo $firstRowOnly;
-		$firstRowOnly="";
-		echo "<td>" . $title . "</td>";
-		echo "<td>" . $domain_label . "</td>";
-		echo "<td>" . $importance_label . "</td>";
-		echo "<td>" . $os_label . "</td>";
-		echo "<td>" . $rest_label . "</td>";
-		echo "</tr>\n";
-	}
+                foreach ( $issue ["labels"] as $label ) {
+                        if (0 === strpos ( $label ["name"], "domain: " )) {
+                                $domain_label = $domain_label . $domain_inbetween . '<SPAN style="background-color: #' . $label ["color"] . ';"> <b>' . htmlspecialchars(substr ( $label ["name"], 8 ))                                      . " </b></SPAN>&ensp;";
+                                $domain_inbetween = "<br>";
+                        } elseif (0 === strpos ( $label ["name"], "importance: " )) {
+                                $importance_label = $importance_label . $importance_inbetween . '<SPAN style="background-color: #' . $label ["color"] . ';"> <b>' . htmlspecialchars(substr ( $label ["n                                     ame"], 12 )) . " </b></SPAN>&ensp;";
+                                $importance_inbetween = "<br>";
+                        } elseif (0 === strpos ( $label ["name"], "OS: " )) {
+                                $os_label = $os_label . $os_inbetween .'<SPAN style="background-color: #' . $label ["color"] . ';"> <b>' . htmlspecialchars(substr ( $label ["name"], 4 )) . " </b></SPA                                     N>&ensp;";
+                                $os_inbetween = "<br>";
+                        } elseif (0 === strpos($label["name"], "status: fixed in")){
+                          // ignore fixed status as this is in first column already
+                        } else {
+                                $rest_label = $rest_label . $rest_inbetween . '<SPAN style="background-color: #' . $label ["color"] . ';"> <b>' . htmlspecialchars($label ["name"]) . " </b></SPAN>&ensp                                     ;";
+                                $rest_inbetween = "<br>";
+                        }
+                }
+                $ret=$ret .  "<tr>";
+                $ret=$ret .  $firstRowOnly;
+                $firstRowOnly="";
+                $ret=$ret .  "<td>" . $title . "</td>";
+                $ret=$ret .  "<td>" . $domain_label . "</td>";
+                $ret=$ret .  "<td>" . $importance_label . "</td>";
+                $ret=$ret .  "<td>" . $os_label . "</td>";
+                $ret=$ret .  "<td>" . $rest_label . "</td>";
+                $ret=$ret .  "</tr>\n";
+        }
+        return $ret;
 }
+
+
+
 
 // read data from local file
 $labelsJson = cachedGithubJson ( "githubdata_labels_cache.tmp", "https://api.github.com/repos/Sloeber/arduino-eclipse-plugin/labels?per_page=100" );
@@ -172,7 +209,7 @@ foreach ( $labels as $label ) {
         $minor=0;
 
         if ($curLabelversion != "nightly" ) {
-            $labelVerArr=split('\.', $curLabelversion);
+            $labelVerArr=explode('.', $curLabelversion);
             $major=$labelVerArr[0];
             $minor=$labelVerArr[1];
         }
@@ -180,7 +217,7 @@ foreach ( $labels as $label ) {
         // this assumes we will never reach minor version 1000 :)
         $sortVersion = ($major*1000)+$minor;
 
-		//store in versions array
+                //store in versions array
         $versions[$sortVersion] = $curLabelversion;
     }
 }
@@ -188,9 +225,12 @@ foreach ( $labels as $label ) {
 // calculate comparison value for user's current build given by parameters
 $myversion=($MAJOR_VERSION*1000)+$MINOR_VERSION;
 
+
+
+
 // start the table
-echo "<table>\n";
-echo "<tr><th>fixed in</th><th>title</th><th>domain</th><th>importance</th><th>os</th><th>other</th></tr>\n";
+$table= "<table>\n";
+$table= $table . "<tr><th>fixed in</th><th>title</th><th>domain</th><th>importance</th><th>os</th><th>other</th></tr>\n";
 
 $totalIssues =0;
 // loop through collected versions
@@ -198,22 +238,35 @@ foreach ($versions as $sortVersion => $strVersion) {
     // filter versions below and including the one we run
     if ($myversion < $sortVersion) {
         // sanitize because we will use this in filename - must not contain / or \ or such...
-		$cacheFile = "githubdata_".preg_replace("/[^0-9a-zA-Z\.]/", "_", $strVersion)."_cache.tmp";
+                $cacheFile = "githubdata_".preg_replace("/[^0-9a-zA-Z\.]/", "_", $strVersion)."_cache.tmp";
 
         $CurIssuesJson = cachedGithubJson ( $cacheFile, "https://api.github.com/repos/Sloeber/arduino-eclipse-plugin/issues?state=all&labels=" . urlencode("status: fixed in ".$strVersion));
         $CurIssues = json_decode ( $CurIssuesJson, true );
 
-		$totalIssues = $totalIssues + count($CurIssues);
-        dumpIssues($strVersion,$CurIssues);
+                $totalIssues = $totalIssues + count($CurIssues);
+        $table= $table . dumpIssues($strVersion,$CurIssues);
     }
 }
 
 if (0 === $totalIssues) {
-	echo "<tr><td colspan=6>There are no issues fixed in later builds </td></tr>\n";
+        $table= $table .  "<tr><td colspan=6>There are no issues fixed in later builds </td></tr>\n";
 }
 
-echo "</table>";
+$table= $table .  "</table>";
 
+//report if data is outdated or incomplete
+if( $enabledump === true){
+    echo "<strong>WARNING: The data below may contain outdated data or may be incomplete.<br> See  the bottom of this page for more details in the debug info.</strong><br>\n";
+    $SHOW_DEBUG_INFO=true;
+}
+echo $table;
+
+//dump debug info if needed
+if($SHOW_DEBUG_INFO){
+echo    "<br>\n";
+echo $debuginfo;
+echo "<strong>end of debug output </strong><br>\n";
+}
 
 ?>
 
@@ -233,4 +286,3 @@ echo "</table>";
 <script src="js/ui-bootstrap-tpls.js"></script>
 <script src="js/app.js"></script>
 </html>
-
