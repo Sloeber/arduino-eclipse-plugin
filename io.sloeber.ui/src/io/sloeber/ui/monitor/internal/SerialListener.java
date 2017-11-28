ba609d631521f5c9abf071710f7fb5fadc0c01a0
jantje <eclipse@baeyens.it>
2017-11-28 12:16:49 +0100

    #873 should also improve #830

--- io.sloeber.ui/src/io/sloeber/ui/monitor/internal/SerialListener.java
+++ io.sloeber.ui/src/io/sloeber/ui/monitor/internal/SerialListener.java
@@ -3,6 +3,7 @@ package io.sloeber.ui.monitor.internal;
 import java.nio.BufferOverflowException;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
+import java.nio.charset.StandardCharsets;
 
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
@@ -18,135 +19,141 @@ public class SerialListener implements MessageConsumer {
 	SerialMonitor theMonitor;
 	boolean isDisposed = false;
 	int theColorIndex;
-	private ByteBuffer myReceivedPlotterData = ByteBuffer.allocate(2000);
+	private ByteBuffer myReceivedSerialData = ByteBuffer.allocate(2000);
 
 	public SerialListener(SerialMonitor monitor, int colorIndex) {
 		this.theMonitor = monitor;
 		this.theColorIndex = colorIndex;
-		this.myReceivedPlotterData.order(ByteOrder.LITTLE_ENDIAN);
-	}
-
-	public int removeBytesFromStart(int n) {
-		if (n == 0) {
-			return this.myReceivedPlotterData.position();
-		}
-		int index = 0;
-		for (int i = 0; i < n; i++)
-			this.myReceivedPlotterData.put(i, (byte) 0);
-		for (int i = n; i < this.myReceivedPlotterData.position(); i++) {
-			this.myReceivedPlotterData.put(index++, this.myReceivedPlotterData.get(i));
-			this.myReceivedPlotterData.put(i, (byte) 0);
-		}
-
-		return this.myReceivedPlotterData.position(index).position();
+		this.myReceivedSerialData.order(ByteOrder.LITTLE_ENDIAN);
 	}
 
 	@Override
 	public void message(byte[] newData) {
+		event(internalMessage(newData));
+	}
+
+	protected String internalMessage(byte[] newData) {
+		String ret = new String();
 		if (myPlotterFilterFlag) {
 			// filter plotter data
 			try {
-				this.myReceivedPlotterData.put(newData);
+				this.myReceivedSerialData.put(newData);
 			} catch (BufferOverflowException e) {
-				this.myReceivedPlotterData.clear();
-				Activator.log(new Status(IStatus.WARNING, Activator.getId(), Messages.serialListenerPlotterSkippingData));
+				this.myReceivedSerialData.clear();
+				Activator.log(
+						new Status(IStatus.WARNING, Activator.getId(), Messages.serialListenerPlotterSkippingData));
+				return ret;
 			}
-			internalExtractAndRemovePlotterData();
+
+			if (this.myReceivedSerialData.remaining() > 4) {
+				this.myReceivedSerialData.flip();
+				ret = internalRemovePlotterData();
+				this.myReceivedSerialData.compact();
+			}
 		} else {
 			// treat data just like a event
 			if (newData[newData.length - 1] == '\r') {
 				newData[newData.length - 1] = ' ';
 			}
-			event(new String(newData));
+			ret = (new String(newData));
 		}
+		return ret;
 	}
 
-	private void internalExtractAndRemovePlotterData() {
+	private String internalRemovePlotterData() {
+		String outMessage = new String();
 
-		String monitorMessage = ""; //$NON-NLS-1$
-		boolean doneSearching = false;
-		int length = this.myReceivedPlotterData.position();
-		int searchPointer;
-		for (searchPointer = 0; (searchPointer < length - 1) && !doneSearching; searchPointer++) {
-			if (this.myReceivedPlotterData.getShort(searchPointer) != Activator.PLOTTER_START_DATA) {
-				char addChar = (char) this.myReceivedPlotterData.get(searchPointer);
-				monitorMessage += Character.toString(addChar);
-			} else {
-				// have we received the full header of the plotter data?
-				if (length < (searchPointer + 6)) {
-					if (searchPointer != 0) {
-						length = removeBytesFromStart(searchPointer);
-					}
-					doneSearching = true;
-					// System.out.println("case 1"); //$NON-NLS-1$
-				} else {
-					int bytestoRead = this.myReceivedPlotterData.getShort(2);
-					if ((bytestoRead < 0) || (bytestoRead > (10 * 2))) {
+		int pos = this.myReceivedSerialData.position();
+		String inMessage = StandardCharsets.US_ASCII.decode(this.myReceivedSerialData).toString();
+		this.myReceivedSerialData.position(pos);
+
+
+		boolean found = false;
+		int lastFound = this.myReceivedSerialData.position();
+		// Scan for plotter data
+		for (int curByte = this.myReceivedSerialData.position(); curByte < this.myReceivedSerialData.limit()
+				- 1; curByte++) {
+			if (this.myReceivedSerialData.getShort(curByte) == Activator.PLOTTER_START_DATA) {
+				// we have a hit.
+				found = true;
+				if ((lastFound != curByte) && (curByte > lastFound)) {
+					outMessage += inMessage.substring(lastFound, curByte - 1);
+				}
+				this.myReceivedSerialData.position(curByte);
+				if (this.myReceivedSerialData.remaining() > 4) {
+					int bytestoRead = this.myReceivedSerialData.getShort(curByte + 2);
+					if ((bytestoRead < 0) || (bytestoRead > 10 * 2)) {
 						Activator.log(
 								new Status(IStatus.WARNING, Activator.getId(), Messages.serialListenerErrorInputPart1
-										+ bytestoRead / 2 + Messages.serialListenerErrorInputPart2));
-						searchPointer += 4;// skip the plotter start and length
-											// so
-						// data is shown
-						// System.out.println("case 2"); //$NON-NLS-1$
+										+ bytestoRead / 2 + ' ' + Messages.serialListenerErrorInputPart2));
 					} else {
-						if ((searchPointer + (bytestoRead + 4)) < length) {
-							searchPointer += (bytestoRead + 4) - 1; // just skip
-							// the
-							// data
-							// System.out.println("case 3"); //$NON-NLS-1$
-						} else // not all data arrived for the latest data set
-						{
-							if (searchPointer != 0) {
-								length = removeBytesFromStart(searchPointer);
+						if (bytestoRead + 4 <= this.myReceivedSerialData.remaining()) {
+							int numChannels = bytestoRead / 2;
+							this.myReceivedSerialData.getShort();
+							this.myReceivedSerialData.getShort();
+							for (int curChannel = 0; curChannel < numChannels; curChannel++) {
+								this.myReceivedSerialData.getShort();
 							}
-							doneSearching = true;
-
-							// System.out.println("case 4"); //$NON-NLS-1$
+							lastFound = this.myReceivedSerialData.position();
+							curByte = lastFound - 1;
+						} else {
+							break;
 						}
 					}
 				}
 			}
 		}
-		if (!doneSearching) {
-			if (searchPointer == length - 1) {
-
-				byte addChar = this.myReceivedPlotterData.get(searchPointer);
-
-				if (addChar != (byte) (Activator.PLOTTER_START_DATA)) {
-					if (addChar == '\r') {
-						addChar = ' ';
-					}
-					searchPointer++;
-					monitorMessage += Character.toString((char) addChar);
-				}
-			}
-			removeBytesFromStart(searchPointer);
+		if ((!found) && (this.myReceivedSerialData.limit() > 1) && (this.myReceivedSerialData
+				.get(this.myReceivedSerialData.limit() - 1) != (Activator.PLOTTER_START_DATA >> 8))) {
+			this.myReceivedSerialData.position(this.myReceivedSerialData.limit());
+			outMessage = inMessage;
 		}
-		event(monitorMessage);
+
+		return outMessage;
 	}
 
 	@Override
 	public void dispose() {
 		this.isDisposed = true;
-		this.myReceivedPlotterData.clear();
+		this.myReceivedSerialData.clear();
 	}
+
+	public class TxtUpdater implements Runnable {
+		private boolean running = false;
+		private String additionalSerialData = new String();
+
+		public synchronized void addData(String event) {
+			if (!event.isEmpty()) {
+				this.additionalSerialData = this.additionalSerialData + event;
+				if (!this.running || this.additionalSerialData.length() > 500) {
+					Display.getDefault().asyncExec(this);
+					this.running = true;
+				}
+			}
+		}
+		private synchronized void synchronizedrun() {
+			try {
+				if (!SerialListener.this.isDisposed) {
+					SerialListener.this.theMonitor.ReportSerialActivity(this.additionalSerialData,
+							SerialListener.this.theColorIndex);
+					this.additionalSerialData = new String();
+				}
+			} catch (Exception e) {// ignore as we get errors when closing
+				// down
+			}
+			this.running = false;
+		}
+		@Override
+		public void run() {
+			synchronizedrun();
+		}
+	};
+
+	TxtUpdater textUpdater = new TxtUpdater();
 
 	@Override
 	public void event(String event) {
-		final String tempString = new String(event);
-		Display.getDefault().asyncExec(new Runnable() {
-			@Override
-			public void run() {
-				try {
-					if (!SerialListener.this.isDisposed) {
-					SerialListener.this.theMonitor.ReportSerialActivity(tempString, SerialListener.this.theColorIndex);
-					}
-				} catch (Exception e) {// ignore as we get errors when closing
-					// down
-				}
-			}
-		});
+		this.textUpdater.addData(event);
 
 	}
 
