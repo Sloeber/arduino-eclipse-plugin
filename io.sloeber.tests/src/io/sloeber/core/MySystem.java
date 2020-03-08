5bb60862d9d0cc70ea7a47a77edfc87f89d3055d
jantje <eclipse@baeyens.it>
2020-03-08 17:03:57 +0100

    Teensy install moved on my system

--- io.sloeber.tests/src/io/sloeber/core/MySystem.java
+++ io.sloeber.tests/src/io/sloeber/core/MySystem.java
@@ -25,7 +25,7 @@ public class MySystem {
 		case jantjesWindowsMachineHashKey:
 		case jantjesWindowsMachineHashkeyAfterUpdate:
 		case jantjeWindowsMachineHashkeyAfterSecondUpdate:
-			return "C:\\test\\teensy-latest\\hardware\\teensy";
+			return "E:\\arduino\\arduino-1.8.12 - teensy";
 		case jantjesLinuxMachineHashKey:
 			return "/home/jan/nas/linux/arduino/arduino-1.8.8-linux64/arduino-1.8.8/hardware/teensy";
 		}
