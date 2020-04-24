cd545e28128b8a422996c34f0544107b1f0420c0
jan <jan@flat.net>
2020-04-24 21:02:42 +0200

    Add issue #555 to compile inoon allboards test

--- io.sloeber.tests/src/templates/CreateAndCompileTest/sketch.cpp
+++ io.sloeber.tests/src/templates/CreateAndCompileTest/sketch.cpp
@@ -1,5 +1,16 @@
 #include "Arduino.h"
 
+#ifdef ARDUINO_BOARD
+char mychar1[] = ARDUINO_BOARD;
+#endif
+
+#ifdef USB_MANUFACTURER
+char mychar2[] = USB_MANUFACTURER;
+#endif
+#ifdef USB_PRODUCT
+char mychar3[] = USB_PRODUCT;
+#endif
+
 
 
 //The setup function is with spaces between the curly braces
