43aa43ddab8f9e803f7c0ce37bafaa3dc4808a6e
jantje <eclipse@baeyens.it>
2020-11-13 15:51:53 +0100

    #1236 removed the whole remind thing

--- io.sloeber.ui/src/io/sloeber/ui/Activator.java
+++ io.sloeber.ui/src/io/sloeber/ui/Activator.java
@@ -3,17 +3,12 @@ package io.sloeber.ui;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
-import org.eclipse.core.runtime.jobs.IJobManager;
 import org.eclipse.core.runtime.jobs.Job;
-import org.eclipse.core.runtime.preferences.IEclipsePreferences;
-import org.eclipse.core.runtime.preferences.InstanceScope;
-import org.eclipse.jface.resource.ImageDescriptor;
-import org.eclipse.jface.resource.ResourceLocator;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.eclipse.ui.progress.UIJob;
 import org.eclipse.ui.statushandlers.StatusManager;
 import org.osgi.framework.BundleContext;
-import org.osgi.service.prefs.BackingStoreException;
+
 import io.sloeber.core.api.LibraryManager;
 import io.sloeber.ui.helpers.MyPreferences;
 import io.sloeber.ui.listeners.MyLibraryInstallHandler;
@@ -27,33 +22,12 @@ import io.sloeber.ui.listeners.ProjectExplorerListener;
  */
 @SuppressWarnings("nls")
 public class Activator extends AbstractUIPlugin {
-	static abstract class FamilyJob extends Job {
-		static final String MY_FAMILY = "myJobFamily"; //$NON-NLS-1$
 
-		public FamilyJob(String name) {
-			super(name);
-		}
-
-		@Override
-		public boolean belongsTo(Object family) {
-			return family == MY_FAMILY;
-		}
-
-	}
 	private static final String PLUGIN_ID = "io.sloeber.core.ui"; //$NON-NLS-1$
 	public static final String NODE_ARDUINO = "io.sloeber.arduino";
 	private static Activator instance;
 	private static BundleContext myContext;
-	// The shared instance
-	private static final String FLAG_START = "F" + "s" + "S" + "t" + "a" + "t" + "u" + "s";
-	private static final String UPLOAD_FLAG = "F" + "u" + "S" + "t" + "a" + "t" + "u" + "s";
-	private static final String BUILD_FLAG = "F" + "b" + "S" + "t" + "a" + "t" + "u" + "s";
-	private static final String LOCAL_FLAG = "l" + FLAG_START;
-	private static final String HELP_LOC = "https://www.baeyens.it/eclipse/remind.php";
-	/**
-	 * This is a wrapper method to quickly make the dough code that is the basis
-	 * of the io.sloeber.core.managers and io.sloeber.core.managers.ui to work.
-	 */
+
 	public static String getId() {
 		return PLUGIN_ID;
 	}
@@ -106,17 +80,6 @@ public class Activator extends AbstractUIPlugin {
 		MyPreferences.setCleanSerialMonitorAfterUpload(MyPreferences.getCleanSerialMonitorAfterUpload());
 	}
 
-	/**
-	 * Returns an image descriptor for the image file at the given plug-in
-	 * relative path
-	 *
-	 * @param path
-	 *            the path
-	 * @return the image descriptor
-	 */
-	public static ImageDescriptor getImageDescriptor(String path) {
-		return ResourceLocator.imageDescriptorFromBundle(PLUGIN_ID, path).orElse(null);
-	}
 
 	public static final short PLOTTER_START_DATA = (short) 0xCDAB;// This is the
 	// 205 171 or
@@ -137,47 +100,7 @@ public class Activator extends AbstractUIPlugin {
 		instance = this;
 		initializeImportantVariables();
 		runGUIRegistration();
-		remind();
-
 	}
 
-	@Override
-	public void stop(BundleContext context) throws Exception {
-		IJobManager jobMan = Job.getJobManager();
-		jobMan.cancel(FamilyJob.MY_FAMILY);
-		jobMan.join(FamilyJob.MY_FAMILY, null);
-	}
 	
-	static void remind() {
-
-		Job job = new FamilyJob("pluginReminder") { //$NON-NLS-1$
-			@Override
-			protected IStatus run(IProgressMonitor monitor) {
-
-				IEclipsePreferences myScope = InstanceScope.INSTANCE.getNode(NODE_ARDUINO);
-				int curFsStatus = myScope.getInt(FLAG_START, 0);
-				int curFuStatus = myScope.getInt(UPLOAD_FLAG, 0);
-				int curFbStatus = myScope.getInt(BUILD_FLAG, 0);
-				int curFsiStatus = curFsStatus + curFuStatus + curFbStatus;
-				int lastFsiStatus = myScope.getInt(LOCAL_FLAG, 0);
-				final int trigger = 30;
-				if ((curFsiStatus - lastFsiStatus) < 0) {
-					lastFsiStatus = curFsiStatus - (trigger + 1);
-				}
-				if ((curFsiStatus - lastFsiStatus) >= trigger) {
-					myScope.putInt(LOCAL_FLAG, curFsiStatus);
-					try {
-						myScope.flush();
-					} catch (BackingStoreException e) {
-						// this should not happen
-					}
-					PleaseHelp.doHelp(HELP_LOC);
-				}
-				remind();
-				return Status.OK_STATUS;
-			}
-		};
-		job.setPriority(Job.DECORATE);
-		job.schedule(60000);
-	}
 }
