57f2688f850ce5cd94f039df6e779aca25a0b94e
jantje <eclipse@baeyens.it>
2020-12-13 18:52:30 +0100

    fix wrong library addition (at least a bit better again)

--- io.sloeber.core/src/io/sloeber/core/listeners/IndexerListener.java
+++ io.sloeber.core/src/io/sloeber/core/listeners/IndexerListener.java
@@ -13,11 +13,8 @@ import org.eclipse.cdt.core.index.IIndexerStateEvent;
 import org.eclipse.cdt.core.index.IIndexerStateListener;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.QualifiedName;
 import org.eclipse.core.runtime.Status;
-import org.eclipse.core.runtime.jobs.Job;
 
 import io.sloeber.core.Activator;
 import io.sloeber.core.Messages;
@@ -63,30 +60,16 @@ public class IndexerListener implements IIndexChangeListener, IIndexerStateListe
         }
     }
 
-    private static void updateLibraries(IProject updateProject) {
-        final QualifiedName PROJECT = new QualifiedName("SLOEBER", "project"); //$NON-NLS-1$ //$NON-NLS-2$
-        Job installLibJob = new Job("Adding Arduino libraries to project " + updateProject.getName()) { //$NON-NLS-1$
-
-            @Override
-            protected IStatus run(IProgressMonitor monitor) {
-                IProject project = (IProject) getProperty(PROJECT);
-                try {
-                    Common.log(new Status(Const.SLOEBER_STATUS_DEBUG, Activator.getId(),
-                            "Looking for libraries for project :" + project.getName())); //$NON-NLS-1$
-                    Libraries.checkLibraries(project);
-
-                } catch (Exception e) {
-                    Common.log(new Status(IStatus.WARNING, Activator.getId(), Messages.Failed_To_Add_Libraries, e));
-                }
-                Common.log(new Status(Const.SLOEBER_STATUS_DEBUG, Activator.getId(),
-                        "libraries added for project " + project.getName())); //$NON-NLS-1$
-                return Status.OK_STATUS;
-            }
-
-        };
-        installLibJob.setProperty(PROJECT, updateProject);
-        installLibJob.setPriority(Job.DECORATE);
-        installLibJob.schedule();
+    private static void updateLibraries(IProject project) {
+        try {
+            Common.log(new Status(Const.SLOEBER_STATUS_DEBUG, Activator.getId(),
+                    "Looking for libraries for project :" + project.getName())); //$NON-NLS-1$
+            Libraries.checkLibraries(project);
+        } catch (Exception e) {
+            Common.log(new Status(IStatus.WARNING, Activator.getId(), Messages.Failed_To_Add_Libraries, e));
+        }
+        Common.log(new Status(Const.SLOEBER_STATUS_DEBUG, Activator.getId(),
+                "libraries added for project " + project.getName())); //$NON-NLS-1$
     }
 
 }
