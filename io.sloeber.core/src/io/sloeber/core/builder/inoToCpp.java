00b99c9a83187bf0803b11574e34230867bf4d74
jantje <eclipse@baeyens.it>
2021-03-19 22:13:47 +0100

    reapply formatting

--- io.sloeber.core/src/io/sloeber/core/builder/inoToCpp.java
+++ io.sloeber.core/src/io/sloeber/core/builder/inoToCpp.java
@@ -15,47 +15,50 @@ import io.sloeber.core.tools.PdePreprocessor;
 public class inoToCpp extends IncrementalProjectBuilder {
 
     class SampleDeltaVisitor implements IResourceDeltaVisitor {
-	/*
-	 * (non-Javadoc)
-	 *
-	 * @see org.eclipse.core.resources.IResourceDeltaVisitor#visit(org.eclipse. core.resources.IResourceDelta)
-	 */
-	@Override
-	public boolean visit(IResourceDelta delta) throws CoreException {
-	    IResource resource = delta.getResource();
-	    if (resource.getFileExtension() != null) {
-		if (resource.getFileExtension().equalsIgnoreCase("ino") //$NON-NLS-1$
-			|| resource.getFileExtension().equalsIgnoreCase("pde")) { //$NON-NLS-1$
-		    try {
-			PdePreprocessor.processProject(true,getProject());
-		    } catch (CoreException e) {
-			e.printStackTrace();
-		    }
-		    return false;
-		}
-	    }
-	    return true;
-	}
+        /*
+         * (non-Javadoc)
+         *
+         * @see org.eclipse.core.resources.IResourceDeltaVisitor#visit(org.eclipse.
+         * core.resources.IResourceDelta)
+         */
+        @Override
+        public boolean visit(IResourceDelta delta) throws CoreException {
+            IResource resource = delta.getResource();
+            if (resource.getFileExtension() != null) {
+                if (resource.getFileExtension().equalsIgnoreCase("ino") //$NON-NLS-1$
+                        || resource.getFileExtension().equalsIgnoreCase("pde")) { //$NON-NLS-1$
+                    try {
+                        PdePreprocessor.processProject(true, getProject());
+                    } catch (CoreException e) {
+                        e.printStackTrace();
+                    }
+                    return false;
+                }
+            }
+            return true;
+        }
     }
 
     /*
      * (non-Javadoc)
      *
-     * @see org.eclipse.core.internal.events.InternalBuilder#build(int, java.util.Map, org.eclipse.core.runtime.IProgressMonitor)
+     * @see org.eclipse.core.internal.events.InternalBuilder#build(int,
+     * java.util.Map, org.eclipse.core.runtime.IProgressMonitor)
      */
     @Override
-    protected IProject[] build(int kind, @SuppressWarnings("rawtypes") Map args, IProgressMonitor monitor) throws CoreException {
-	if (kind == FULL_BUILD) {
-	    fullBuild(monitor);
-	} else {
-	    IResourceDelta delta = getDelta(getProject());
-	    if (delta == null) {
-		fullBuild(monitor);
-	    } else {
-		incrementalBuild(delta, monitor);
-	    }
-	}
-	return null;
+    protected IProject[] build(int kind, @SuppressWarnings("rawtypes") Map args, IProgressMonitor monitor)
+            throws CoreException {
+        if (kind == FULL_BUILD) {
+            fullBuild(monitor);
+        } else {
+            IResourceDelta delta = getDelta(getProject());
+            if (delta == null) {
+                fullBuild(monitor);
+            } else {
+                incrementalBuild(delta, monitor);
+            }
+        }
+        return null;
     }
 
     @Override
@@ -64,18 +67,18 @@ public class inoToCpp extends IncrementalProjectBuilder {
     }
 
     @SuppressWarnings("unused")
-	protected void fullBuild(final IProgressMonitor monitor) {
-	try {
-	    PdePreprocessor.processProject(false,getProject());
-	} catch (CoreException e) {
-	    e.printStackTrace();
-	}
+    protected void fullBuild(final IProgressMonitor monitor) {
+        try {
+            PdePreprocessor.processProject(false, getProject());
+        } catch (CoreException e) {
+            e.printStackTrace();
+        }
     }
 
     @SuppressWarnings("unused")
-	protected void incrementalBuild(IResourceDelta delta, IProgressMonitor monitor) throws CoreException {
-	// the visitor does the work.
-	delta.accept(new SampleDeltaVisitor());
+    protected void incrementalBuild(IResourceDelta delta, IProgressMonitor monitor) throws CoreException {
+        // the visitor does the work.
+        delta.accept(new SampleDeltaVisitor());
 
     }
 }
