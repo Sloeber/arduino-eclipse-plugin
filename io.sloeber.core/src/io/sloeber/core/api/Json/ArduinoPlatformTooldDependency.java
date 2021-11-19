4184d288674ce5ee46e5522deb06af8aac2c3f83
jantje <eclipse@baeyens.it>
2021-11-19 03:48:20 +0100

    #1411 take the packager into account when installing a platform

--- io.sloeber.core/src/io/sloeber/core/api/Json/ArduinoPlatformTooldDependency.java
+++ io.sloeber.core/src/io/sloeber/core/api/Json/ArduinoPlatformTooldDependency.java
@@ -1,16 +1,21 @@
 package io.sloeber.core.api.Json;
 
 import static io.sloeber.core.Gson.GsonConverter.*;
+import static io.sloeber.core.common.Const.*;
+
+import org.eclipse.core.runtime.IPath;
 
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
 
 import io.sloeber.core.api.VersionNumber;
+import io.sloeber.core.common.ConfigurationPreferences;
 
 public class ArduinoPlatformTooldDependency {
 
     private String myName;
+    private String myPackager;
     private VersionNumber myVersion;
 
     private transient ArduinoPlatformVersion myParentPlatform;
@@ -22,6 +27,7 @@ public class ArduinoPlatformTooldDependency {
         try {
 
             myName = getSafeString(jsonObject, "name");
+            myPackager = getSafeString(jsonObject, "packager");
             myVersion = getSafeVersion(jsonObject, "version");
         } catch (Exception e) {
             throw new JsonParseException("failed to parse json  " + e.getMessage());
@@ -29,11 +35,20 @@ public class ArduinoPlatformTooldDependency {
     }
 
     public String getName() {
-        return this.myName;
+        return myName;
     }
 
     public VersionNumber getVersion() {
         return myVersion;
     }
 
+    public String getPackager() {
+        return myPackager;
+    }
+
+    public IPath getInstallPath() {
+        return ConfigurationPreferences.getInstallationPathPackages().append(myPackager).append(TOOLS).append(myName)
+                .append(myVersion.toString());
+
+    }
 }
