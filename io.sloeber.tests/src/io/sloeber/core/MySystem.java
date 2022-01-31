b8da59073a40cb89e855f74e99951e09141f6757
jantje <eclipse@baeyens.it>
2022-01-31 16:01:41 +0100

    build failed on github due to trying to run teensy tests

--- io.sloeber.tests/src/io/sloeber/core/MySystem.java
+++ io.sloeber.tests/src/io/sloeber/core/MySystem.java
@@ -20,12 +20,14 @@ public class MySystem {
     private static final String currentMachine = getMachine();
 
     private static String getMachine() {
-        if (Common.isWindows) {
-            if ("jan".equals(System.getProperty("user.name"))) {
+
+        if ("jan".equals(System.getProperty("user.name"))) {
+            if (Common.isWindows) {
                 return jantjesWindowsMachine;
             }
+            return jantjesLinuxMachine;
         }
-        return jantjesLinuxMachine;
+        return new String();
     }
 
     public static String getTeensyPlatform() {
@@ -46,30 +48,30 @@ public class MySystem {
     public static MCUBoard[] getUploadBoards() {
         switch (currentMachine) {
         case jantjesLinuxMachine: {
-        	/* using this udev file thins work for most but not all boards (fried 101 )
-        	  KERNEL=="ttyACM*", ATTRS{idVendor}=="2341", ATTRS{idProduct}=="003d", SYMLINK+="ttyDueProg"
-KERNEL=="ttyACM*", ATTRS{idVendor}=="2341", ATTRS{idProduct}=="0041", SYMLINK+="ttyYun"
-KERNEL=="ttyACM*", ATTRS{idVendor}=="2341", ATTRS{idProduct}=="8041", SYMLINK+="ttyYun"
-KERNEL=="ttyACM*", ATTRS{idVendor}=="2341", ATTRS{idProduct}=="0043", SYMLINK+="ttyUno"
-KERNEL=="ttyACM*", ATTRS{idVendor}=="2341", ATTRS{idProduct}=="0042", SYMLINK+="ttyMega2560"
-KERNEL=="ttyACM*", ATTRS{idVendor}=="2341", ATTRS{idProduct}=="0010", SYMLINK+="ttyMega2560"
-KERNEL=="ttyACM*", ATTRS{idVendor}=="2341", ATTRS{idProduct}=="8036", SYMLINK+="ttyLeonardo"
-KERNEL=="ttyACM*", ATTRS{idVendor}=="2341", ATTRS{idProduct}=="0036", SYMLINK+="ttyLeonardo"
-KERNEL=="ttyACM*", ATTRS{idVendor}=="1B4F", ATTRS{idProduct}=="9207", SYMLINK+="ttyFried"
-KERNEL=="ttyACM*", ATTRS{idVendor}=="1B4F", ATTRS{idProduct}=="9208", SYMLINK+="ttyFried"
-KERNEL=="ttyACM*", ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="2157", SYMLINK+="ttyZeroProg"
-KERNEL=="ttyACM*", ATTRS{idVendor}=="2341", ATTRS{idProduct}=="804d", SYMLINK+="ttyZeroNa"
-KERNEL=="ttyACM*", ATTRS{idVendor}=="2341", ATTRS{idProduct}=="004d", SYMLINK+="ttyZeroNa"
-
-        	 */
+            /* using this udev file thins work for most but not all boards (fried 101 )
+              KERNEL=="ttyACM*", ATTRS{idVendor}=="2341", ATTRS{idProduct}=="003d", SYMLINK+="ttyDueProg"
+            KERNEL=="ttyACM*", ATTRS{idVendor}=="2341", ATTRS{idProduct}=="0041", SYMLINK+="ttyYun"
+            KERNEL=="ttyACM*", ATTRS{idVendor}=="2341", ATTRS{idProduct}=="8041", SYMLINK+="ttyYun"
+            KERNEL=="ttyACM*", ATTRS{idVendor}=="2341", ATTRS{idProduct}=="0043", SYMLINK+="ttyUno"
+            KERNEL=="ttyACM*", ATTRS{idVendor}=="2341", ATTRS{idProduct}=="0042", SYMLINK+="ttyMega2560"
+            KERNEL=="ttyACM*", ATTRS{idVendor}=="2341", ATTRS{idProduct}=="0010", SYMLINK+="ttyMega2560"
+            KERNEL=="ttyACM*", ATTRS{idVendor}=="2341", ATTRS{idProduct}=="8036", SYMLINK+="ttyLeonardo"
+            KERNEL=="ttyACM*", ATTRS{idVendor}=="2341", ATTRS{idProduct}=="0036", SYMLINK+="ttyLeonardo"
+            KERNEL=="ttyACM*", ATTRS{idVendor}=="1B4F", ATTRS{idProduct}=="9207", SYMLINK+="ttyFried"
+            KERNEL=="ttyACM*", ATTRS{idVendor}=="1B4F", ATTRS{idProduct}=="9208", SYMLINK+="ttyFried"
+            KERNEL=="ttyACM*", ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="2157", SYMLINK+="ttyZeroProg"
+            KERNEL=="ttyACM*", ATTRS{idVendor}=="2341", ATTRS{idProduct}=="804d", SYMLINK+="ttyZeroNa"
+            KERNEL=="ttyACM*", ATTRS{idVendor}=="2341", ATTRS{idProduct}=="004d", SYMLINK+="ttyZeroNa"
+            
+             */
             MCUBoard[] boards = { Teensy.teensypp2().setUploadPort("/dev/ttyACM0"),
                     Arduino.leonardo().setUploadPort("/dev/ttyLeonardo"), //werkt niet
                     Arduino.fried2016().setUploadPort("/dev/ttyFried"), //werkt niet
                     Arduino.zeroNatviePort().setUploadPort("/dev/ttyZeroNa"), //werkt niet
                     Arduino.yun().setUploadPort("/dev/ttyYun"), ESP8266.wemosD1().setUploadPort("/dev/ttyUSB0"),
-                    Arduino.arduino_101().setUploadPort("/dev/ttyArduino_101"), 
+                    Arduino.arduino_101().setUploadPort("/dev/ttyArduino_101"),
                     Arduino.zeroProgrammingPort().setUploadPort("/dev/ttyZeroProg"),
-                    Arduino.mega2560Board().setUploadPort("/dev/ttyMega2560"), 
+                    Arduino.mega2560Board().setUploadPort("/dev/ttyMega2560"),
                     Arduino.dueprogramming().setUploadPort("/dev/ttyDueProg"),
                     Arduino.uno().setUploadPort("/dev/ttyUno"), };
             return boards;
@@ -83,8 +85,8 @@ KERNEL=="ttyACM*", ATTRS{idVendor}=="2341", ATTRS{idProduct}=="004d", SYMLINK+="
                     Arduino.yun().setUploadPort("COM106"), ESP8266.wemosD1().setUploadPort("COM108"),
                     Arduino.arduino_101().setUploadPort("COM110"),
                     Arduino.zeroProgrammingPort().setUploadPort("COM111"),
-                    Arduino.mega2560Board().setUploadPort("COM112"),
-                    Arduino.dueprogramming().setUploadPort("COM124"), Arduino.uno().setUploadPort("COM126"), };
+                    Arduino.mega2560Board().setUploadPort("COM112"), Arduino.dueprogramming().setUploadPort("COM124"),
+                    Arduino.uno().setUploadPort("COM126"), };
             return boards;
         default:
             fail("Boards for the system with haskey " + currentMachine + " are not found");
