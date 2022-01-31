59f17b0cc905623c00bc26f71615095b566deeb0
jantje <jan@baeyens.it>
2022-01-31 04:40:02 +0100

    Added udev rules for my boards for upload testing

--- io.sloeber.tests/src/io/sloeber/core/MySystem.java
+++ io.sloeber.tests/src/io/sloeber/core/MySystem.java
@@ -25,7 +25,7 @@ public class MySystem {
                 return jantjesWindowsMachine;
             }
         }
-        return new String();
+        return jantjesLinuxMachine;
     }
 
     public static String getTeensyPlatform() {
@@ -33,7 +33,7 @@ public class MySystem {
         case jantjesWindowsMachine:
             return "E:\\arduino\\arduino-1.8.12 - teensy\\hardware\\teensy";
         case jantjesLinuxMachine:
-            return "/home/jan/tensyduino/arduino-1.8.12/hardware/teensy";
+            return "/home/jan/teensyduino/arduino-1.8.12/hardware/teensy";
         default:
             return new String();
         }
@@ -46,14 +46,32 @@ public class MySystem {
     public static MCUBoard[] getUploadBoards() {
         switch (currentMachine) {
         case jantjesLinuxMachine: {
+        	/* using this udev file thins work for most but not all boards (fried 101 )
+        	  KERNEL=="ttyACM*", ATTRS{idVendor}=="2341", ATTRS{idProduct}=="003d", SYMLINK+="ttyDueProg"
+KERNEL=="ttyACM*", ATTRS{idVendor}=="2341", ATTRS{idProduct}=="0041", SYMLINK+="ttyYun"
+KERNEL=="ttyACM*", ATTRS{idVendor}=="2341", ATTRS{idProduct}=="8041", SYMLINK+="ttyYun"
+KERNEL=="ttyACM*", ATTRS{idVendor}=="2341", ATTRS{idProduct}=="0043", SYMLINK+="ttyUno"
+KERNEL=="ttyACM*", ATTRS{idVendor}=="2341", ATTRS{idProduct}=="0042", SYMLINK+="ttyMega2560"
+KERNEL=="ttyACM*", ATTRS{idVendor}=="2341", ATTRS{idProduct}=="0010", SYMLINK+="ttyMega2560"
+KERNEL=="ttyACM*", ATTRS{idVendor}=="2341", ATTRS{idProduct}=="8036", SYMLINK+="ttyLeonardo"
+KERNEL=="ttyACM*", ATTRS{idVendor}=="2341", ATTRS{idProduct}=="0036", SYMLINK+="ttyLeonardo"
+KERNEL=="ttyACM*", ATTRS{idVendor}=="1B4F", ATTRS{idProduct}=="9207", SYMLINK+="ttyFried"
+KERNEL=="ttyACM*", ATTRS{idVendor}=="1B4F", ATTRS{idProduct}=="9208", SYMLINK+="ttyFried"
+KERNEL=="ttyACM*", ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="2157", SYMLINK+="ttyZeroProg"
+KERNEL=="ttyACM*", ATTRS{idVendor}=="2341", ATTRS{idProduct}=="804d", SYMLINK+="ttyZeroNa"
+KERNEL=="ttyACM*", ATTRS{idVendor}=="2341", ATTRS{idProduct}=="004d", SYMLINK+="ttyZeroNa"
+
+        	 */
             MCUBoard[] boards = { Teensy.teensypp2().setUploadPort("/dev/ttyACM0"),
-                    Arduino.leonardo().setUploadPort("/dev/ttyS0"), //werkt niet
-                    Arduino.fried2016().setUploadPort("/dev/ttyS0"), //werkt niet
-                    Arduino.zeroNatviePort().setUploadPort("/dev/ttyS0"), //werkt niet
-                    Arduino.yun().setUploadPort("COM20"), ESP8266.wemosD1().setUploadPort("/dev/ttyUSB0"),
-                    Arduino.arduino_101().setUploadPort("COM15"), Arduino.zeroProgrammingPort().setUploadPort("COM14"),
-                    Arduino.mega2560Board().setUploadPort("COM11"), Arduino.dueprogramming().setUploadPort("COM8"),
-                    Arduino.uno().setUploadPort("COM6"), };
+                    Arduino.leonardo().setUploadPort("/dev/ttyLeonardo"), //werkt niet
+                    Arduino.fried2016().setUploadPort("/dev/ttyFried"), //werkt niet
+                    Arduino.zeroNatviePort().setUploadPort("/dev/ttyZeroNa"), //werkt niet
+                    Arduino.yun().setUploadPort("/dev/ttyYun"), ESP8266.wemosD1().setUploadPort("/dev/ttyUSB0"),
+                    Arduino.arduino_101().setUploadPort("/dev/ttyArduino_101"), 
+                    Arduino.zeroProgrammingPort().setUploadPort("/dev/ttyZeroProg"),
+                    Arduino.mega2560Board().setUploadPort("/dev/ttyMega2560"), 
+                    Arduino.dueprogramming().setUploadPort("/dev/ttyDueProg"),
+                    Arduino.uno().setUploadPort("/dev/ttyUno"), };
             return boards;
         }
         case jantjesWindowsMachine:
