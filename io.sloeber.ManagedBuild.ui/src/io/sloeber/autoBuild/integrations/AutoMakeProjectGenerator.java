9af4e54e4aafbf710c2fddab1226b240b39f8b1e
jantje <eclipse@baeyens.it>
2022-12-10 19:04:07 +0100

    rename class #1126

--- io.sloeber.ManagedBuild.ui/src/io/sloeber/autoBuild/integrations/AutoMakeProjectGenerator.java
+++ io.sloeber.ManagedBuild.ui/src/io/sloeber/autoBuild/integrations/AutoMakeProjectGenerator.java
@@ -1,62 +1,33 @@
-/*******************************************************************************
- * Copyright (c) 2015 QNX Software Systems and others.
- *
- * This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License 2.0
- * which accompanies this distribution, and is available at
- * https://www.eclipse.org/legal/epl-2.0/
- *
- * SPDX-License-Identifier: EPL-2.0
- *******************************************************************************/
+
 package io.sloeber.autoBuild.integrations;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
-import org.eclipse.cdt.core.CCProjectNature;
-import org.eclipse.cdt.core.CProjectNature;
-import org.eclipse.cdt.core.build.CBuilder;
 import org.eclipse.cdt.core.model.CoreModel;
 import org.eclipse.cdt.core.model.IPathEntry;
-import org.eclipse.core.resources.ICommand;
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IProjectDescription;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.Path;
-import org.eclipse.tools.templates.freemarker.FMProjectGenerator;
+import org.eclipse.tools.templates.core.IGenerator;
 import org.eclipse.tools.templates.freemarker.SourceRoot;
 import org.eclipse.tools.templates.freemarker.TemplateManifest;
-import org.osgi.framework.Bundle;
 
-import io.sloeber.autoBuild.ui.internal.Activator;
+public class AutoMakeProjectGenerator implements IGenerator {
 
-public class CMakeProjectGenerator extends FMProjectGenerator {
+    public AutoMakeProjectGenerator() {
 
-    public CMakeProjectGenerator(String manifestFile) {
-        super(manifestFile);
     }
 
     @Override
-    protected void initProjectDescription(IProjectDescription description) {
-        description.setNatureIds(
-                new String[] { CProjectNature.C_NATURE_ID, CCProjectNature.CC_NATURE_ID, Activator.getId() });
-        ICommand command = description.newCommand();
-        CBuilder.setupBuilder(command);
-        description.setBuildSpec(new ICommand[] { command });
-    }
-
-    @Override
-    public Bundle getSourceBundle() {
-        return Activator.getPlugin().getBundle();
-    }
-
-    @Override
-    public void generate(Map<String, Object> model, IProgressMonitor monitor) throws CoreException {
-        super.generate(model, monitor);
+    public void generate(IProgressMonitor monitor) throws CoreException {
+        // TODO Auto-generated method stub
+        IGenerator.super.generate(monitor);
 
         List<IPathEntry> entries = new ArrayList<>();
         IProject project = getProject();
@@ -86,6 +57,32 @@ public class CMakeProjectGenerator extends FMProjectGenerator {
                 new IPath[] { new Path("**/CMakeFiles/**") })); //$NON-NLS-1$
         CoreModel.getDefault().create(project).setRawPathEntries(entries.toArray(new IPathEntry[entries.size()]),
                 monitor);
+    }
+
+    @Override
+    public IFile[] getFilesToOpen() {
+        // TODO Auto-generated method stub
+        return IGenerator.super.getFilesToOpen();
+    }
+
+    private TemplateManifest getManifest() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    private IProject getProject() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public void setProjectName(String projectName) {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void setLocationURI(URI locationURI) {
+        // TODO Auto-generated method stub
+
     }
 
 }
