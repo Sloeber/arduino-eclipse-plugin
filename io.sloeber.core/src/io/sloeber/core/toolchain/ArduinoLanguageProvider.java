e01e31790c1d515ace7581fe1ea10c3053d46b4b
jantje <eclipse@baeyens.it>
2023-08-31 01:43:10 +0200

    add a tofix for memory stuff #1126

--- io.sloeber.core/src/io/sloeber/core/toolchain/ArduinoLanguageProvider.java
+++ io.sloeber.core/src/io/sloeber/core/toolchain/ArduinoLanguageProvider.java
@@ -6,6 +6,7 @@ import java.util.LinkedList;
 import java.util.List;
 
 import org.eclipse.cdt.core.language.settings.providers.ILanguageSettingsProvider;
+import org.eclipse.cdt.core.language.settings.providers.LanguageSettingsStorage;
 import org.eclipse.cdt.core.settings.model.CIncludePathEntry;
 import org.eclipse.cdt.core.settings.model.ICConfigurationDescription;
 import org.eclipse.cdt.core.settings.model.ICLanguageSettingEntry;
@@ -15,36 +16,40 @@ import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 
-public class ArduinoLanguageProvider implements  ILanguageSettingsProvider
-{
+public class ArduinoLanguageProvider implements ILanguageSettingsProvider {
 
-	@Override
-	public String getId() {
-		return "io.sloeber.languageSettingsProvider"; //$NON-NLS-1$
-	}
+    @Override
+    public String getId() {
+        return "io.sloeber.languageSettingsProvider"; //$NON-NLS-1$
+    }
 
-	@Override
-	public String getName() {
-		return "ArduinoLanguageProvider"; //$NON-NLS-1$
-	}
+    @Override
+    public String getName() {
+        return "ArduinoLanguageProvider"; //$NON-NLS-1$
+    }
 
-	@Override
-	public List<ICLanguageSettingEntry> getSettingEntries(ICConfigurationDescription cfgDescription, IResource rc,
-			String languageId) {
-		if(languageId==null||languageId.isBlank()) {
-			return null;
-		}
-		List<ICLanguageSettingEntry>ret= new LinkedList<>();
-		IProject project =rc.getProject();
-		//JABA TOFIX : I hard coded this to make things work to see this is the way to go
-		IFolder coreFolder=project.getFolder("core/core");
-		IFolder variansFolder=project.getFolder("core/variant");
-		int flags =ICSettingEntry.READONLY|ICSettingEntry.VALUE_WORKSPACE_PATH;
-		ret.add(CDataUtil.getPooledEntry(new CIncludePathEntry(coreFolder, flags)));
-		ret.add(CDataUtil.getPooledEntry(new CIncludePathEntry(variansFolder, flags)));
-		
-		return ret;
-	}
+    @Override
+    public List<ICLanguageSettingEntry> getSettingEntries(ICConfigurationDescription cfgDescription, IResource rc,
+            String languageId) {
+        if (languageId == null || languageId.isBlank()) {
+            return null;
+        }
+        List<ICLanguageSettingEntry> ret = new LinkedList<>();
+        IProject project = rc.getProject();
+        //JABA TOFIX : I hard coded this to make things work to see this is the way to go
+        IFolder coreFolder = project.getFolder("core/core");
+        IFolder variansFolder = project.getFolder("core/variant");
+        int flags = ICSettingEntry.READONLY | ICSettingEntry.VALUE_WORKSPACE_PATH;
+        ret.add(CDataUtil.getPooledEntry(new CIncludePathEntry(coreFolder, flags)));
+        ret.add(CDataUtil.getPooledEntry(new CIncludePathEntry(variansFolder, flags)));
+        /*
+         * TOFIX
+         * @return the list of setting entries or {@code null} if no settings defined.
+                 *    The list needs to be a pooled list created by {@link LanguageSettingsStorage#getPooledList(List)}
+                 *    to save memory and avoid deep equality comparisons.
+                 */
+        return ret;
+    }
 
     //    @Override
     //    protected String getCompilerCommand(String languageId) {
