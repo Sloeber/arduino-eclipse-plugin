9e7c4e0342de73be62bd0a0114b0e0321c1365b3
jantje <eclipse@baeyens.it>
2023-10-12 22:12:38 +0200

    remove unused field myIsvalid #1126

--- io.sloeber.ManagedBuild/src/io/sloeber/autoBuild/integration/BuildTargetPlatformData.java
+++ io.sloeber.ManagedBuild/src/io/sloeber/autoBuild/integration/BuildTargetPlatformData.java
@@ -24,7 +24,6 @@ public class BuildTargetPlatformData extends CTargetPlatformData {
     private String[] myBinaryParserIds;
     private String myId;
     private String myName;
-    private boolean myIsValid;
 
     public BuildTargetPlatformData(ITargetPlatform iTargetPlatform) {
         fTargetPlatform = (TargetPlatform) iTargetPlatform;
@@ -32,12 +31,10 @@ public class BuildTargetPlatformData extends CTargetPlatformData {
             myBinaryParserIds = fTargetPlatform.getBinaryParserList().toArray(new String[0]);
             myId = CDataUtil.genId(fTargetPlatform.getId());
             myName = fTargetPlatform.getName();
-            myIsValid = true;
         } else {
             myBinaryParserIds = new String[0];
             myId = CDataUtil.genId("my.stupid.id.because.i.fake.existance.but.in.matters.of.fact.I.do.not.exist"); //$NON-NLS-1$
             myName = "to be or not to be; that is the questions"; //$NON-NLS-1$
-            myIsValid = false;
         }
     }
 
