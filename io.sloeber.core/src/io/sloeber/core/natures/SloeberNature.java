ddf5c19d0ee65daf9f41739eacab72ea4178b1a6
jan <jan@192.168.1.194>
2024-02-28 21:29:02 +0100

    refactor rename arduinoNature to SloeberNature

--- io.sloeber.core/src/io/sloeber/core/natures/SloeberNature.java
+++ io.sloeber.core/src/io/sloeber/core/natures/SloeberNature.java
@@ -1,6 +1,5 @@
 package io.sloeber.core.natures;
 
-import org.eclipse.core.resources.ICommand;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IProjectDescription;
 import org.eclipse.core.resources.IProjectNature;
@@ -10,9 +9,9 @@ import org.eclipse.core.runtime.NullProgressMonitor;
 
 import io.sloeber.core.api.Const;
 
-public class ArduinoNature implements IProjectNature {
+public class SloeberNature implements IProjectNature {
     public static final String NATURE_ID = Const.SLOEBER_NATURE_ID;
-    private static final String BUILDER_ID = "io.sloeber.core.inoToCpp"; //$NON-NLS-1$
+    private IProject myProject = null;
 
     public static void addNature(IProject project, IProgressMonitor inMonitor) throws CoreException {
         IProgressMonitor monitor = inMonitor;
@@ -33,23 +32,10 @@ public class ArduinoNature implements IProjectNature {
         monitor.done();
     }
 
-    private IProject myProject = null;
 
     @Override
     public void configure() throws CoreException {
-        IProjectDescription description = myProject.getDescription();
-        // add builder to project
-        ICommand command = description.newCommand();
-        ICommand[] commands = description.getBuildSpec();
-        command.setBuilderName(BUILDER_ID);
-        ICommand[] newCommands = new ICommand[commands.length + 1];
-
-        // Add it before other builders.
-        System.arraycopy(commands, 0, newCommands, 1, commands.length);
-        newCommands[0] = command;
-        description.setBuildSpec(newCommands);
-
-        myProject.setDescription(description, new NullProgressMonitor());
+    	//JABA nothing to do here
     }
 
     @Override
